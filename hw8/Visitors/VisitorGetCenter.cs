c9fb7c9dd185ade8bb7d33c323f3e053b968f400
AnastasiaSD <32881045+AnastasiaSD@users.noreply.github.com>
2019-12-09 15:53:52 +0500

    Update VisitorGetCenter.cs

--- hw8/Visitors/VisitorGetCenter.cs
+++ hw8/Visitors/VisitorGetCenter.cs
@@ -5,13 +5,13 @@ namespace hw8
 {
     public class VisitorGetCenter : IVisitor
     {
-        public void VisitCircle(Circle circle)
+        public void Visit(Circle circle)
         {
             Console.WriteLine(circle);
             Console.WriteLine($"And my center is {circle.Center}");
         }
 
-        public void VisitTriangle(Triangle tgl)
+        public void Visit(Triangle tgl)
         {
             var centerX = (tgl.Point1.X + tgl.Point2.X + tgl.Point3.X) / 3;
             var centerY = (tgl.Point1.Y + tgl.Point2.Y + tgl.Point3.Y) / 3;
@@ -19,7 +19,7 @@ namespace hw8
             Console.WriteLine($"And my center is ({centerX},{centerY})");
         }
 
-        public void VisitRectangle(Rectangle rectangle)
+        public void Visit(Rectangle rectangle)
         {
             var pointDiag = new Point(rectangle.TopLeftPoint.X + rectangle.Width,
                 rectangle.TopLeftPoint.Y - rectangle.Height);
